bec1c36e28ab68b949901b5453cbd9a026375abb
xhesica <23373296@buaa.edu.cn>
2025-09-02 15:46:40 +0800

    use vpn

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import os
 from login import login
 from selectCourse import selectCourse
 
-login_url="https://byxk.buaa.edu.cn/xsxk/profile/index.html"
+login_url="https://d.buaa.edu.cn/https/77726476706e69737468656265737421f2ee599769327d517f468ca88d1b203b/xsxk/profile/index.html"
 
 if __name__=='__main__':
     print("自动选课准备中")
